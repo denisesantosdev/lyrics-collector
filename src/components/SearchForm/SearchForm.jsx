e14baf58e84763c2be44ff7ef947aef5f42be299
Denise Santos <denisesantosdev@gmail.com>
2024-04-22 19:29:49 -0300

    move search form to a component

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { fetchLyrics } from "../../services/lyrics-api";
+import {lyricsDataContext} from "../../context/LyricsDataContext";
 
 const SearchForm = () => {
   const [searchQuery, setSearchQuery] = useState({});
+  const [formIsSubmitted, setFormIsSubmitted] = useState(false);
+
+  const {lyricsData,setLyricsData} = useContext(lyricsDataContext)
 
   function handleOnSubmit(event) {
     event.preventDefault();
     setFormIsSubmitted(true);
-  }
-
-  if (formIsSubmitted) {
-    setFormIsSubmitted(false);
-    setSearchQuery({});
   }
 
   const handleOnChange = (event) => {
@@ -21,6 +21,42 @@ const SearchForm = () => {
   };
 
   //console.log(searchQuery);
+
+  useEffect(() => {
+    async function searchLyrics() {
+      if (formIsSubmitted) {
+        try {
+          const lyricsApiData = await fetchLyrics(
+            searchQuery.songTitle,
+            searchQuery.artistName
+          );
+
+          if (Object.keys(lyricsApiData).length === 0) {
+            throw new Error(404);
+          }
+
+          //console.log(lyricsApiData);
+          setLyricsData({
+            artistName: lyricsApiData.artist,
+            songTitle: lyricsApiData.title,
+            albumImage: lyricsApiData.image,
+            lyrics: lyricsApiData.lyrics,
+          });
+
+          setFormIsSubmitted(false);
+          setSearchQuery({});
+        } catch (err) {
+          if (err.message === "404") {
+            console.log("not found");
+          } else {
+            console.log(err.message);
+          }
+        }
+      }
+    }
+
+    searchLyrics();
+  }, [formIsSubmitted]);
 
   return (
     <form onSubmit={handleOnSubmit}>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,71 +1,15 @@
 import React, { useContext, useEffect, useState } from "react";
-import { fetchLyrics } from "../services/lyrics-api";
 import { lyricsDataContext } from "../context/LyricsDataContext";
 import SavedLyrics from "../components/SavedLyrics/SavedLyrics"
 import SongLyrics from "../components/SongLyrics/SongLyrics";
 import SignOutBtn from "../components/SignOutBtn/SignOutBtn"
+import SearchForm from "../components/SearchForm/SearchForm";
 
 
 const Home = ({isUserLoggedIn}) => {
   const { lyricsData, setLyricsData } = useContext(lyricsDataContext);
 
-  const [searchQuery, setSearchQuery] = useState({
-    songName: "",
-    artistName: "",
-  });
-
-  const [formIsSubmitted, setFormIsSubmitted] = useState(false);
-
-  const handleOnChange = (event) => {
-    setSearchQuery({
-      ...searchQuery,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  function handleOnSubmit(event) {
-    event.preventDefault();
-    setFormIsSubmitted(true);
-  }
-
-  //console.log(searchQuery,formIsSubmitted);
-
-  useEffect(() => {
-    async function searchLyrics() {
-      if (formIsSubmitted) {
-        try {
-          const lyricsApiData = await fetchLyrics(
-            searchQuery.songName,
-            searchQuery.artistName
-          );
-
-          if (Object.keys(lyricsApiData).length === 0) {
-            throw new Error(404);
-          }
-
-          setLyricsData({
-            artistName: lyricsApiData.artist,
-            songTitle: lyricsApiData.title,
-            albumImage: lyricsApiData.image,
-            lyrics: lyricsApiData.lyrics,
-          });
-
-          setFormIsSubmitted(false);
-          setSearchQuery({});
-        } catch (err) {
-          if (err.message === "404") {
-            console.log("not found");
-          } else {
-            console.log(err.message);
-          }
-        }
-      }
-    }
-
-    searchLyrics();
-  }, [formIsSubmitted]);
-
-  return (
+   return (
     <>
       <header>
       {isUserLoggedIn ? (
@@ -78,23 +22,8 @@ const Home = ({isUserLoggedIn}) => {
       )}
     </header>
       <section>
-        <form onSubmit={handleOnSubmit}>
-          <input
-            type="text"
-            placeholder="Song Name"
-            name="songName"
-            onChange={handleOnChange}
-            required={true}
-          />
-          <input
-            type="text"
-            placeholder="Artist Name"
-            name="artistName"
-            onChange={handleOnChange}
-            required={true}
-          />
-          <button>Search</button>
-        </form>
+        <SearchForm/>
+        
         {lyricsData ? <SongLyrics /> : <h1>Search for your favorite songs</h1>}
       </section>
 
