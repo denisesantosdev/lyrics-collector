c961cc386128099a2608ad267c273d2d8562177d
Denise Santos <denisesantosdev@gmail.com>
2024-05-21 20:41:49 -0300

    fix undefined email when using google login

--- src/customHooks/useAuth.jsx
+++ src/customHooks/useAuth.jsx
@@ -6,6 +6,7 @@ import {
   signInWithPopup,
   signOut,
   onAuthStateChanged,
+  GoogleAuthProvider
 } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { ToastContext } from "../context/ToastContext";
@@ -65,6 +66,7 @@ function useAuth() {
   const authSignInWithGoogle = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
+        console.log(result);
         // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
@@ -80,7 +82,7 @@ function useAuth() {
         const errorCode = error.code;
         const errorMessage = error.message;
         // The email of the user's account used.
-        const email = error.customData.email;
+      //  const email = error.customData.email;
         // The AuthCredential type that was used.
         const credential = GoogleAuthProvider.credentialFromError(error);
         // ...
